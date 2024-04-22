58abdb6247ba98f689a0825bade209180cac93e9
InvarGervi123 <68227462+InvarGervi123@users.noreply.github.com>
2024-04-23 00:31:25 +0300

    test8

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,4 +1,17 @@
-const ChatsPage = () => {
-    return <div className="background">chats...</div>;
-  };
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic} from 'react-chat-engine-advanced'
+
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic(
+        'cbe864c5-7702-4cfa-a07f-07f8519c8f4f',
+        props.user.username,
+        props.user.secret
+    );
+    return (
+         <div style={{ height: '100vh' }}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow {...chatProps} style={{ height: '100%'}} />
+        </div>
+    )
+  }
+
   export default ChatsPage;
